0d50b8a9c3579819384eb4d6c8ab4d1dc0064e3e
Tiktokcity <danhskipper18@gmail.com>
2020-12-16 14:08:01 +0700

    fourth commit with add new orderController and OrderServiceInterface

--- src/main/java/com/dev/fshop/controller/OrderController.java
+++ src/main/java/com/dev/fshop/controller/OrderController.java
@@ -1,7 +1,41 @@
 package com.dev.fshop.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.dev.fshop.entity.OrderDetailEntity;
+import com.dev.fshop.entity.OrderItemEntity;
+import com.dev.fshop.services.OrderServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/f-shop/v1/api")
 public class OrderController {
+    @Autowired
+    private OrderServiceInterface orderServiceInterface;
+
+    @GetMapping(path = "/orders/{orderId}")
+    public List<OrderItemEntity> findListOrderItemByOrderId(@PathVariable String orderId) {
+        return orderServiceInterface.findListOrderItemByOrderId(orderId);
+    }
+
+    @PostMapping(path = "/orders")
+    public OrderDetailEntity createNewOrder(@RequestBody OrderDetailEntity orderDetailEntity) {
+        return orderServiceInterface.createNewOrder(orderDetailEntity);
+    }
+
+    @PatchMapping(path = "/orders/orderitems/{orderItemId}/products/{quantity}")
+    public OrderDetailEntity updateQuantityProduct(@PathVariable Integer quantity, @PathVariable Integer orderItemId) {
+        return orderServiceInterface.updateQuantityProduct(quantity, orderItemId);
+    }
+
+    @DeleteMapping(path = "/orders")
+    public boolean deleteOrderByCustomer(@RequestParam(name = "userId")String userId, @RequestBody List<OrderDetailEntity> orderDetailEntities) {
+        return orderServiceInterface.deleteOrderByCustomer(userId, orderDetailEntities);
+    }
+
+    @DeleteMapping(path = "/orders/orderItems/{orderItemId}")
+    public boolean deleteItemInOrder(@PathVariable Integer orderItemId) {
+        return orderServiceInterface.deleteItemInOrder(orderItemId);
+    }
 }
--- src/main/java/com/dev/fshop/repositories/OrderDetailReposity.java
+++ src/main/java/com/dev/fshop/repositories/OrderDetailReposity.java
@@ -2,7 +2,6 @@ package com.dev.fshop.repositories;
 
 
 import com.dev.fshop.entity.OrderDetailEntity;
-import com.dev.fshop.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
