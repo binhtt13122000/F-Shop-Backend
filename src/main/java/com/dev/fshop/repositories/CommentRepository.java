de099488e46af6f3d0aea2144be4ccf996719b98
nhanlt1602 <nhanlt16235@gmail.com>
2020-12-16 18:29:04 +0700

    delete findCommentbyId commit

--- src/main/java/com/dev/fshop/repositories/CommentRepository.java
+++ src/main/java/com/dev/fshop/repositories/CommentRepository.java
@@ -16,9 +16,6 @@ public interface CommentRepository extends JpaRepository<CommentEntity, Integer>
     @Query("Select v.commentId, v.content, v.proId   from CommentEntity v where v.name = :name ")
     public List<CommentEntity> findCommentByName(String name);
 
-    @Query("Select v.commentId, v.content, v.proId  from CustomerEntity u inner join CommentEntity v on u.userId = v.userId ")
-    public List<CommentEntity> findCommentById(String userId);
-
     @Query("delete from CommentEntity u where u.commentId = :commentId ")
     public boolean deleteComment( Integer commentId);
 
