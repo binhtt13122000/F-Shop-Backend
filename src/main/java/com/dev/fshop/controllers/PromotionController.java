4a48175c5c389bb37f3583a92ea733aff5d0e02b
BinhTT1312 <binhtt.13122000@gmail.com>
2020-12-19 23:52:17 +0700

    add api for promotion

--- src/main/java/com/dev/fshop/controllers/PromotionController.java
+++ src/main/java/com/dev/fshop/controllers/PromotionController.java
@@ -1,7 +1,15 @@
 package com.dev.fshop.controllers;
 
+import com.dev.fshop.entities.CommentEntity;
 import com.dev.fshop.entities.PromotionEntity;
 import com.dev.fshop.services.PromotionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +18,51 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/v1/api")
+@Tag(name = "Promotion")
 public class PromotionController {
     @Autowired
     private PromotionService promotionService;
 
-
-    @GetMapping(path = "/promotions/{promotionId}")
-    public ResponseEntity<PromotionEntity> findPromotionById(@PathVariable String promotionId) {
-        return ResponseEntity.ok().body(promotionService.findPromotionByPromotionId(promotionId));
-    }
-
+    //get promotion by user
+    @Operation(description = "See promotions of user (ALL ROLE)", responses = {
+            @ApiResponse(
+                    description = "Get Successfully!",
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = CommentEntity.class)))
+            ),
+            @ApiResponse(
+                    description = "Access deny!",
+                    responseCode = "403",
+                    content = @Content(
+                            mediaType = "String",
+                            examples = @ExampleObject(
+                                    description = "Display when user is not authorized!",
+                                    value = "Access deny!"),
+                            schema = @Schema(implementation = String.class))
+            ),
+            @ApiResponse(
+                    description = "Not found!",
+                    responseCode = "404",
+                    content = @Content(
+                            mediaType = "String",
+                            examples = @ExampleObject(
+                                    description = "Display when this user is not found!",
+                                    value = "User is not found!"),
+                            schema = @Schema(implementation = String.class))
+            ),
+            @ApiResponse(
+                    description = "Server throw Exception!",
+                    responseCode = "500",
+                    content = @Content(
+                            mediaType = "String",
+                            examples = @ExampleObject(
+                                    description = "Server throw Exception!",
+                                    value = "Server throw Exception!"),
+                            schema = @Schema(implementation = String.class))
+            )
+    })
     @GetMapping(path = "/promotions")
     public ResponseEntity<List<PromotionEntity>> getAllPromotionsByUserId(@RequestParam(name = "userId") String userId) {
         return  ResponseEntity.ok().body(promotionService.getAllPromotionsByUserId(userId));
--- src/main/java/com/dev/fshop/entities/PromotionEntity.java
+++ src/main/java/com/dev/fshop/entities/PromotionEntity.java
@@ -1,6 +1,8 @@
 package com.dev.fshop.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,23 +15,40 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
-
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "Promotion")
+@Schema(name = "Promotion")
 public class PromotionEntity {
 
     @Id
+    @JsonIgnore
     @Column(name = "promotionID", nullable = false, unique = true)
     private String promotionID;
+
     @Column(name = "promotionName")
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
     private String promotionName;
 
+
     @Column(name = "promo")
+    @NotNull
+    @NotBlank
+    @Min(10)
+    @Max(80)
     private float promo;
+
     @Column(name = "status")
     private boolean status;
 
@@ -37,5 +56,9 @@ public class PromotionEntity {
     @JoinColumn(name = "userId")
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
+    @JsonIgnore
     private CustomerEntity customerEntity;
+
+    @Transient
+    private String userId;
 }
