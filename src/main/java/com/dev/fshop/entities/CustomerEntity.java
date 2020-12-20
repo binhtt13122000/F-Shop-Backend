432a7be252181b0d4f3619ab0acc2d440519d9ca
BinhTT1312 <binhtt.13122000@gmail.com>
2020-12-20 09:38:16 +0700

    add document for user

--- src/main/java/com/dev/fshop/entities/CustomerEntity.java
+++ src/main/java/com/dev/fshop/entities/CustomerEntity.java
@@ -2,6 +2,7 @@ package com.dev.fshop.entities;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,6 +17,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.JoinColumn;
 import javax.persistence.CascadeType;
+import javax.persistence.Transient;
+import javax.validation.constraints.*;
 import java.util.Collection;
 import java.util.Date;
 
@@ -24,36 +27,64 @@ import java.util.Date;
 @Data
 @Entity
 @Table(name = "Customer")
+@Schema(name = "User")
 public class CustomerEntity {
     @Id
+    @NotBlank
+    @NotNull
+    @Size(max = 40)
     @Column(name = "userId", nullable = false, unique = true)
     private String userId;
 
     @Column(name = "name")
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
     private String name;
 
     @Column(name = "birthDate")
+    @NotBlank
+    @NotNull
     private Date birthDate;
 
     @Column(name = "phoneNumber")
+    @NotBlank
+    @NotNull
+    @Size(max = 15)
     private String phoneNumber;
 
     @Column(name = "email", nullable = false, unique = true)
+    @NotBlank
+    @NotNull
+    @Email
+    @Size(max = 50)
     private String email;
 
     @Column(name = "gender")
+    @NotBlank
+    @NotNull
     private boolean gender;
 
     @Column(name = "country")
+    @NotBlank
+    @NotNull
+    @Size(max = 50)
     private String country;
 
     @Column(name = "address")
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
     private String address;
 
     @Column(name = "registeredAt")
+    @NotNull
+    @NotBlank
     private Date registeredAt;
 
     @Column(name = "lastLogin")
+    @NotNull
+    @NotBlank
     private  Date lastLogin;
 
     @Column(name = "password")
@@ -64,29 +95,41 @@ public class CustomerEntity {
     private String avatar;
 
     @Column(name = "status")
+    @NotNull
+    @NotBlank
     private boolean status;
 
+    @Transient
+    @NotNull
+    @NotBlank
+    @Min(1)
+    @Max(3)
+    private int roleId;
 
     @OneToMany(mappedBy = "customerEntity", cascade = CascadeType.ALL)
     @EqualsAndHashCode.Exclude // không sử dụng trường này trong equals và hashcode
     @ToString.Exclude // Khoonhg sử dụng trong toString()
+    @JsonIgnore
     private Collection<CommentEntity> commentEntities;
 
 
     @OneToMany(mappedBy = "customerEntity", cascade = CascadeType.ALL)
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
+    @JsonIgnore
     private Collection<OrdersEntity> ordersEntities;
 
 
     @OneToMany(mappedBy = "customerEntity", cascade = CascadeType.ALL)
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
+    @JsonIgnore
     private Collection<PromotionEntity> promotionEntities;
 
     @ManyToOne
     @JoinColumn(name = "roleId")
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
+    @JsonIgnore
     private RoleEntity roleEntity;
 }
