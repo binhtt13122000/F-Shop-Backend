f27eb9b0ffb509b1d1136a1e5746c7d5405ab10c
BinhTT1312 <binhtt.13122000@gmail.com>
2020-12-22 12:31:08 +0700

    add api_document for promotion

--- src/main/java/com/dev/fshop/controllers/PromotionController.java
+++ src/main/java/com/dev/fshop/controllers/PromotionController.java
@@ -21,4 +21,152 @@ import java.util.List;
 @Tag(name = "Promotion")
 public class PromotionController {
 
+    @Operation(description = "get promotion by user", responses = {
+            @ApiResponse(
+                    description = "get promotion by user successfully!",
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Promotion.class))
+                    )
+            ),
+            @ApiResponse(
+                    description = "Access denied!",
+                    responseCode = "403",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "Access denied!",
+                                    value = "Access denied!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+            @ApiResponse(
+                    description = "User is not available!",
+                    responseCode = "400",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "User is not available!",
+                                    value = "User is not available!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+            @ApiResponse(
+                    description = "Not found!",
+                    responseCode = "404",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "Not found!",
+                                    value = "Not found!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+    })
+    @GetMapping("/users/{username}/promotions")
+    public ResponseEntity findPromotionByAccount(@PathVariable String username){
+        return null;
+    }
+
+    @Operation(description = "Create new promotion", responses = {
+            @ApiResponse(
+                    description = "Create new promotion successfully!",
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "Create new promotion successfully!",
+                                    value = "Create new promotion successfully!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+            @ApiResponse(
+                    description = "Account is not available!",
+                    responseCode = "404",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "Account is not available!",
+                                    value = "Account is not available!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+            @ApiResponse(
+                    description = "Create failed!",
+                    responseCode = "400",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "Create failed!",
+                                    value = "Create failed!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+    })
+    @PostMapping("/users/{username}/promotions")
+    public ResponseEntity createPromotion(@PathVariable String username){
+        return null;
+    }
+
+    @Operation(description = "update promotion", responses = {
+            @ApiResponse(
+                    description = "update promotion successfully!",
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "update promotion successfully!",
+                                    value = "update promotion successfully!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+            @ApiResponse(
+                    description = "Access denied!",
+                    responseCode = "403",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "Access denied!",
+                                    value = "Access denied!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+            @ApiResponse(
+                    description = "Account is not available!",
+                    responseCode = "404",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "Account is not available!",
+                                    value = "Account is not available!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+            @ApiResponse(
+                    description = "Update failed!",
+                    responseCode = "400",
+                    content = @Content(
+                            mediaType = "text/plain; charset=utf-8",
+                            examples = @ExampleObject(
+                                    description = "Update failed!",
+                                    value = "Update failed!"
+                            ),
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+    })
+    @PutMapping("/users/{username}/promotions")
+    public ResponseEntity updatePromotion(@PathVariable String username){
+        return null;
+    }
 }
